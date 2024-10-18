de77d588b97960d56f8d1ceb8041d4d26d1bd1de
ImogenWren <imogen.wren91@gmail.com>
2024-10-18 12:30:09 +0100

    added to readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # jsonMessenger_library
  Library for easy configuration of a JSON based Serial messaging system for Arduino
+
+_This library has been developed to simplify the implementation of transfer of control data from a user interface, over Serial connections to embedded projects running state machines._
+
+It works by associating different keywords used to define commands sent by a user or user interface, with a list of known states, that could represent all, or some of the various states of a state machine, and 
+a list of the expected variable types to be sent along with the defined keywords.
+
+
+
+ ## Use
+ This library is configured for different users entirely within the `jsonConfig.h` file.
+
+
+
+
--- jsonMessenger_library.ino
+++ jsonMessenger_library.ino
@@ -1,12 +1,24 @@
-//#include "PCA9685.h"
-//#include <Wire.h>
+/* jsonMessenger Library
+
+_Library to simplify two way communication over Serial interfaces using JSON formatted messages_
+
+Imogen Heard
+18/10/24
+
+
+Version V0.0.0
+- Focus on receiving & parsing incoming Serial messages in JSON format
+
+
+*/
+
 
 #include "jsonMessenger.h"
 #include <autoDelay.h>
-//#include <ArduinoSTL.h>  // [Arduino Library Manager]
-//#include <iostream>      // [std::iostream]
+#include <ArduinoSTL.h>  // [Arduino Library Manager]
+#include <iostream>      // [std::iostream]
 
-jsonMessenger jsonRX; // create a json messenger object to handle commands received over Serial connection
+jsonMessenger jsonRX;  // create a json messenger object to handle commands received over Serial connection
 
 autoDelay printDelay;
 /*
@@ -22,7 +34,7 @@ struct jsonStateData {
 void setup() {
   Serial.begin(115200);
 
-  jsonRX.jsonBegin();              
+  jsonRX.jsonBegin();
   Serial.println("end of setup");
 }
 
@@ -33,10 +45,13 @@ void loop() {
 
   jsonStateData nextState = jsonRX.jsonReadSerialLoop();
 
-  if (nextState.cmd_received) {     // If command is received 
+  if (nextState.cmd_received) {  // If command is received
     char buffer[64];
     char databuffer[8];
-    if (nextState.data_type == INTEGER) { // Example of how to deal with different datatypes returned from jsonMessenger object
+    if (nextState.data_type == INTEGER) {       // Example of how to deal with different datatypes returned from jsonMessenger object
+      itoa(nextState.numeric, dataBuffer, 10);  // 10: base 10 // If integer copy integer to string
+    } else if (nextState.data_type == ){
+
     }
     //
     //dtostrf(nextState.data, 2, 2, databuffer);
